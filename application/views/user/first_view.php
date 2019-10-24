55ed58344bfcf7f5cde8cb2878fd375a4e170238
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-24 23:20:50 +0100

    added the first view file

---
+++ application/views/user/first_view.php
@@ -0,0 +1,7 @@
+<div class='w3-container w3-center'>
+  
+<span class="w3-large w3-text-indigo">Welcome </span>
+
+<a class="w3-button w3-indio w3-large"> href="<?=site_url("questions/start") ?>">Start</a>
+
+</div>
