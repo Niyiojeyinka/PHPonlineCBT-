67789017963b34cfaa5c0d843403cc5d53a1cea8
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-25 18:13:22 +0100

    changed env

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost:3306',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'cbt',
