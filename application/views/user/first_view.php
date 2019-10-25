f1c9a0745a60daddfdcffda06af7651b7952b4b5
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-25 18:16:57 +0100

    added continue info

--- application/views/user/first_view.php
+++ application/views/user/first_view.php
@@ -1,7 +1,9 @@
 <div class='w3-container w3-center'>
   
 <span class="w3-large w3-text-indigo">Welcome </span>
-
+<br></br>
+<span class="w3-medium">Click the button below to continue </span>
+<br></br>
 <a class="w3-button w3-indio w3-large" href="<?=site_url("questions/start") ?>">Start</a>
 
 </div>
