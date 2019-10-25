bc8beb4f954f6a24c451fdb997232639000d6405
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-25 18:01:07 +0100

    rewrite the wrong link tag

--- application/views/user/first_view.php
+++ application/views/user/first_view.php
@@ -2,6 +2,6 @@
   
 <span class="w3-large w3-text-indigo">Welcome </span>
 
-<a class="w3-button w3-indio w3-large"> href="<?=site_url("questions/start") ?>">Start</a>
+<a class="w3-button w3-indio w3-large" href="<?=site_url("questions/start") ?>">Start</a>
 
 </div>
