537f90a09976c66f2af4c4f47e605448f326ed60
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-25 19:34:18 +0100

    removed note routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['notes/(:any)'] = 'notes/view/$1';
+
 $route['waitlist'] = 'users/waitlist';
 $route['default_controller'] = 'page';
 $route['404_override'] = '';
