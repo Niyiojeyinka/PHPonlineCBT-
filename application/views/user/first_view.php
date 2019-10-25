0cecfb262d43ae7f7e49ce41dd1870143f365f9b
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-25 18:22:45 +0100

    added border to start button

--- application/views/user/first_view.php
+++ application/views/user/first_view.php
@@ -5,6 +5,6 @@
 <span class="w3-medium">Click the button below to continue </span>
 <br></br>
 <a class="w3-button w3-indigo w3-text-white w3-large w3-hover-text-indigo
-w3-hover-white" href="<?=site_url("question/start") ?>">Start</a>
+w3-hover-white w3-border w3-border-indigo" href="<?=site_url("question/start") ?>">Start</a>
 
 </div>
