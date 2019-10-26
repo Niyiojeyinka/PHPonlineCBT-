07dd9f254615bd3715813082a5c188b33db63038
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-26 22:09:19 +0100

    added form action

--- application/views/admin/create_test_session_view.php
+++ application/views/admin/create_test_session_view.php
@@ -3,6 +3,7 @@
 <span>Create a new test session</span>
 
 <br></br>
+<?=form_open_multipart("admin/create_test_session") ?>
 <label for="test_name" class="w3-label">Test Name / Subject</label><br>
 <input type='text' name='test_name' class='w3-padding'/>
 <br>
