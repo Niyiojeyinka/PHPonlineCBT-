231657f570192a6a9df3d7d411626d433287726c
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-26 21:59:54 +0100

    added the time start input

--- application/views/admin/create_test_session_view.php
+++ application/views/admin/create_test_session_view.php
@@ -5,7 +5,10 @@
 <br></br>
 <label for="test_name" class="w3-label">Test Name / Subject</label><br>
 <input type='text' name='test_name' class='w3-padding'/>
-
+<br>
+<label for="test_start" class="w3-label">Time Start</label><br>
+<input type='text' name='test_start' class='w3-padding'/>
+<br>
 
 
 
