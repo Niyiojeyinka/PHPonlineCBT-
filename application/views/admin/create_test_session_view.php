4ae74a485170423e4a04f93d67fc1e37183fd285
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-26 21:57:49 +0100

    added label for testname

--- application/views/admin/create_test_session_view.php
+++ application/views/admin/create_test_session_view.php
@@ -3,7 +3,7 @@
 <span>Create a new test session</span>
 
 <br></br>
-
+<label for="test_name" class="w3-label">Test Name / Subject</label><br>
 <input type='text' name='test_name' class='w3-padding'/>
 
 
