7e749c1307b7e0c5c6ada10256af52c29b384a02
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-04-06 20:30:41 +0100

    worked on the pre test view

--- application/views/user/pre_start_view.php
+++ application/views/user/pre_start_view.php
@@ -1,11 +1,15 @@
 <div class='w3-container w3-center'>
 
+<br>
 
-<br><br>
+<span class="w3-large w3-text-grey"><strong><?=$next_test['name']?></strong></span>
 
-<span class="w3-large"><?=var_dump($next_test)?></span>
+<br>
 
-<a href='' class='w3-button w3-indigo'>Start Now</a>
+<?=$next_test['instructions']?>
+<br>
+<span class="w3-small">ALL Test must have been submitted before <?=date("F,j Y ,g:ia",$next_test['test_end'])?></span><br>
+<a href='' class='w3-button w3-indigo w3-margin'>Start Now</a>
 
 
 </div>
