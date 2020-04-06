9e8a2457466bb5ccf66d88962c137921849f229e
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-04-06 19:34:24 +0100

    formated the question class

--- application/controllers/Question.php
+++ application/controllers/Question.php
@@ -13,21 +13,21 @@ Date:Start ReWriting  2018
 
 public function __construct()
 {
-     parent::__construct();
+            parent::__construct();
 
-    $this->load->model(array('question_model','users_model','dashboard_model','pages_model'));
-         $this->load->helper(array('url','form','question_helper','page_helper'));
-     $this->load->library(array('form_validation','session'));
-//user login check here
+            $this->load->model(array('question_model','users_model','dashboard_model','pages_model'));
+            $this->load->helper(array('url','form','question_helper','page_helper'));
+            $this->load->library(array('form_validation','session'));
+            //user login check here
 
-      if (!isset($this->session->id) || !isset($this->session->logged_in))
-       {      show_page('users/login') ;    }
+            if (!isset($this->session->id) || !isset($this->session->logged_in))
+            {      show_page('users/login') ;    }
 
 
-$this->siteName ="CBT";
-$this->descriptions ="Examnation Software";
-$this->author ="author Name";
-$this->keywords ="keywords";
+            $this->siteName ="CBT";
+            $this->descriptions ="Examnation Software";
+            $this->author ="author Name";
+            $this->keywords ="keywords";
 
 
 }
@@ -42,19 +42,19 @@ public function index($slug = null)
             $data["title"] = $this->siteName." | Computer Based Test";
             $data["keywords"] =$this->keywords;
             $data["author"] =$this->author;
-           $data["descriptions"] =$this->descriptions;
-           $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-       $data['user_details'] = $this->users_model->get_user_by_id();
+            $data["descriptions"] =$this->descriptions;
+            $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
+            $data['user_details'] = $this->users_model->get_user_by_id();
 
 
 
-      $this->load->view('common/headmeta_view',$data);
-          $this->load->view('user/common/users_nav_view',$data);
-          $this->load->view('common/header_view',$data);
-          $this->load->view('user/common/pre_content_view',$data);
-           $this->load->view('user/first_view',$data);
-           $this->load->view('user/common/post_content_view',$data);
-      $this->load->view('common/footer_view',$data);
+            $this->load->view('common/headmeta_view',$data);
+            $this->load->view('user/common/users_nav_view',$data);
+            $this->load->view('common/header_view',$data);
+            $this->load->view('user/common/pre_content_view',$data);
+            $this->load->view('user/first_view',$data);
+            $this->load->view('user/common/post_content_view',$data);
+            $this->load->view('common/footer_view',$data);
 }
 
 
@@ -62,104 +62,24 @@ public function index($slug = null)
 
 public function start(){
 
-	$data["title"] = $this->siteName." | Pre Start";
-               $data["keywords"] ="";
-               $data["author"] =$this->author;
-              $data["descriptions"] =$this->descriptions;
-           $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-           $data['user_details'] = $this->users_model->get_user_by_id();
-
-	
-	$this->load->view('common/headmeta_view',$data);
-              $this->load->view('user/common/users_nav_view',$data);
-              $this->load->view('common/header_view',$data);
-              $this->load->view('user/common/pre_content_view',$data);
-               $this->load->view('user/pre_start_view',$data);
-               $this->load->view('user/common/post_content_view',$data);
-          $this->load->view('common/footer_view',$data);
-
-
-
-}
-
-public function question_page()
-{
-
-
-          $data["title"] = $this->siteName." | Examination";
-               $data["keywords"] ="";
-               $data["author"] =$this->author;
-              $data["descriptions"] =$this->descriptions;
-           $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-           $data['user_details'] = $this->users_model->get_user_by_id();
-
-
-          $this->load->view('common/headmeta_view',$data);
-              $this->load->view('user/common/users_nav_view',$data);
-              $this->load->view('common/header_view',$data);
-              $this->load->view('user/common/pre_content_view',$data);
-               $this->load->view('user/question_view',$data);
-               $this->load->view('user/common/post_content_view',$data);
-          $this->load->view('common/footer_view',$data);
-
-
-
-}
-
- public function question_act()
-{
-
-   
-//if isset submit
-if(isset($_POST['submit']))
-{
-//mark and show,save
-
-  $_SESSION['submit'] = TRUE;
-  $_SESSION['stopped_time'] = time();
-
-show_page('question/submit');
-
-
-
-}
-
-
-
-}
-
-public function submit()
-{
-$_SESSION['score'] = 0;
-
-    
-//save result to db later here
-//check here if to release result immediately or not
-            $data["title"] = $this->siteName." |  Results";
-            $data["keywords"] =$this->keywords;
+	        $data["title"] = $this->siteName." | Pre Start";
+            $data["keywords"] ="";
             $data["author"] =$this->author;
-           $data["descriptions"] =$this->descriptions;
+            $data["descriptions"] =$this->descriptions;
+            $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
+            $data['user_details'] = $this->users_model->get_user_by_id();
+            $data['next_test']= $this->dashboard_model->get_next_test();
 
-       $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-       $data['items'] =   json_decode($this->users_model->get_user_by_id()['subjects']);
-       $data['user_details'] = $this->users_model->get_user_by_id();
-
-
-      $this->load->view('common/headmeta_view',$data);
-          $this->load->view('user/common/users_nav_view',$data);
-          $this->load->view('common/header_view',$data);
-          $this->load->view('user/common/pre_content_view',$data);
-          $this->load->view('user/result_view',$data);
-          $this->load->view('user/common/post_content_view',$data);
-      $this->load->view('common/footer_view',$data);
-
-
-
-
-
-
+            $this->load->view('common/headmeta_view',$data);
+            $this->load->view('user/common/users_nav_view',$data);
+            $this->load->view('common/header_view',$data);
+            $this->load->view('user/common/pre_content_view',$data);
+            $this->load->view('user/pre_start_view',$data);
+            $this->load->view('user/common/post_content_view',$data);
+            $this->load->view('common/footer_view',$data);
 
 }
+
 
 
 }
