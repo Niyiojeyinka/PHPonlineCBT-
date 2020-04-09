0caf1c90db357ad39c795c8fba582232940c8d58
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-04-09 13:28:02 +0100

    removed old use case files

--- application/views/user/timeup_view.php
+++
@@ -1,6 +0,0 @@
-<div style="display:none" class="w3-center" id="t_div">
-<span class="w3-large w3-text-red">Time Up!!!</span><br>
-<p>Your Assigned Exam Duration is Up.Please Click here
-  to view result</p>
-
-</div>
